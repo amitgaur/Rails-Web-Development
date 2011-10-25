0e391f6e3275abd7eab1e8100b9d180bd91f4f70
Amit Gaur <amitgaur@gmail.com>
2011-10-25 10:13:17 -0700

    more stuff

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+ test "product attributes must not be empty" do
+   product = Product.new
+   assert product.invalid?
+   assert product.errors[:title].any?
+ end
+
+           test "product price must be positive" do
+product = Product.new(:title => "My Book Title",
+                        :description => "yyy",
+                        :image_url   => "zzz.jpg")
+  product.price = -1
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01",
+    product.errors[:price].join('; ')
+
+           end
+
+  end
