501edf54ad7c7290361cd145886f16102d76b96f
Amit Gaur <amitgaur@gmail.com>
2011-10-26 17:18:37 -0700

    Cart controller init

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023015209) do
+ActiveRecord::Schema.define(:version => 20111027001305) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
-    t.string "title"
-    t.text "description"
-    t.string "image_url"
-    t.decimal "price", :precision => 8, :scale => 2
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
