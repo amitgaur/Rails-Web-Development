99ea7a2eb90a3afc5b5ab758e2117d09c2f9017d
Amit Gaur <amitgaur@gmail.com>
2011-10-27 17:44:58 -0700

    Line Item for Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,18 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, :price, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 
 end
