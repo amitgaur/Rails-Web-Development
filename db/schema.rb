a3d5574d947bd932d3d3fcd40ca8dfbd7af0496d
Amit Gaur <amitgaur@gmail.com>
2011-11-30 14:44:28 -0800

    Order creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111028210803) do
+ActiveRecord::Schema.define(:version => 20111123004844) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(:version => 20111028210803) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   :default => 1
+  end
+
+  create_table "orders", :force => true do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "products", :force => true do |t|
