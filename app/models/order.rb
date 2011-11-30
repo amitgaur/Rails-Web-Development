601e3341a41d53d7f221e006fc8b23152bbf4cc5
Amit Gaur <amitgaur@gmail.com>
2011-11-30 14:56:58 -0800

    bundle exec annotate orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,15 @@
 class Order < ActiveRecord::Base
 end
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  address    :text
+#  email      :string(255)
+#  pay_type   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -5,3 +5,16 @@ class OrderTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+# == Schema Information
+#
+# Table name: orders
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  address    :text
+#  email      :string(255)
+#  pay_type   :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
